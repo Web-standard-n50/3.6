24c4cc0998c5bec5527905df019ee2e07e34930f
MJurayev <jurayevmansurbek667@gmail.com>
2022-08-18 11:30:26 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,180 @@
+'use strict';
+// var number1 = prompt("Yoshingizni kiriting:");
+
+// console.log(typeof Number(number1));
+// var number2 = String(13);
+// console.log(number2);
+// console.log(typeof number2);
+
+// Arifmetik amallar.
+// var number1 = 10;
+// var number2 = 30;
+
+// console.log(number1  + number2 + "" + 12);
+// console.log(number2 - number1); // Infinity. -Infinity, NaN
+// console.log(number2/number1);
+// console.log(number1 * number2);
+// console.log(30**2);
+
+// console.log(number2%number1);
+
+// console.log(number2%2===1);
+
+// number1 /=15;
+
+// var a = 13;
+// a = a + 1; // normal
+// a+=1; // syntax sugar 
+// a++; // post increment
+// ++a; // pre increment
+
+// a = a -1; // normal
+// a-=1; // syntax sugar
+// a--; // post decrement
+// --a; // pre decrement
+
+
+// console.log(a--);
+// console.log(a);
+
+
+// console.log(a++ + a++);
+// console.log(a);
+            // 13 + 15
+// console.log(++a + a++);
+// console.log(a);
+
+// console.log('B'+'a' + ('a' - 0) + 'a')
+
+
+// var firstname = "G'ulom";
+// var lastname = "Toshpo'latov"
+// var text = `Wellcome ${firstname}  ${lastname} `;
+// console.log(text);
+// var a = [];
+
+// var number3 = 10.598172893761726488172;
+
+// console.log(number3);
+// console.log(Math.floor(number3));
+// console.log(Math.ceil(number3));
+// console.log(Math.round(number3));
+
+// console.log(number3.toFixed(3) - 0);
+
+// console.log(Math.E)
+// console.log(Math.PI)
+// console.log(Math.abs(10))
+// console.log(Math.sqrt(4))
+
+// console.log(M)
+
+// console.log(Math.random()*10+5)
+// // []
+// console.log(Math.random()*(100-30) + 30)
+// // [30,100]
+// [0,1]
+// var a = 13;
+// var b = 89;
+
+// console.log(Math.round(Math.random()*(b-a) + a));
+
+// [0,1]
+// [0, b-a];
+// [a, b];
+
+
+
+// var a = prompt("Ixtiyoriy son kiriting:");
+// if(a%2 == 0){
+    // alert("Bu  juft son")
+    // bu true qiymat uchun ishlaydi
+// }else{
+    // alert("Bu toq son")
+// }
+
+
+// var number1 = 10;
+// var number2 = "9";
+// console.log(number1 < number2);
+
+// if(number1 >= number2){
+//     console.log("1-son katta")
+// }else{
+//     console.log("2-son katta")
+// }
+
+// console.log(number1 !== number2);
+var isRegistred = false;
+var isAdmin = false + 0;
+
+// if(isRegistred && isAdmin){
+//     console.log("Admin panelga ruhsat bor")
+// }else{
+//     console.log("Admin panel kirolmaysiz.")
+// }
+
+// if(isRegistred || isAdmin){
+//     console.log("foydalanuvchi profiliga ruhsat bor")
+// }else{
+//     console.log("foydalanuvchi profiliga kirolmaysiz.")
+// }
+
+
+// var credentials = {
+//     email:"example@gmail.com",
+//     password:"123"
+// }
+
+// var email = prompt('Email kiriting:');
+// var password = prompt("Parol kiriting:")
+// if(email == credentials.email && password == credentials.password){
+//         alert("Profil")
+//    }else{
+//     alert("login yoki parol noto'g'ri")
+//    }
+
+// var isAdmin = false;
+// if(!isAdmin){
+//     console.log("Siz admin emassiz");
+//     // alert("")
+// }
+// else 
+//     console.log("Siz adminsiz")
+
+// navigator
+// document
+// 
+// divEl.textContent = "Hello World";
+// divEl.style.color = "red";
+// divEl.className = 'text';
+// console.log(window.divEl);
+
+// var divEl = document.querySelectorAll("div");
+
+// console.log(divEl)
+// var oddNumber = prompt("Ixtiyoriy son kiriting:", 0);
+// if(oddNumber % 2 ==0){   
+//     divEl.style.backgroundColor = "red";
+//     divEl.textContent = "Kiritilgan son juft";
+// }else{
+//     divEl.style.backgroundColor = "blue";
+//     divEl.textContent = "Kiritilgan son toq"
+
+// }
+
+
+const USD_TO_UZS = 1090
+
+// var balance = prompt("So'm kiriting:");
+// var price = 12;
+
+
+// if(balance/ USD_TO_UZS > price){
+//     balance -=price * USD_TO_UZS;
+//     alert("Xaridingiz uchun tashakkur!!!");
+// }else{
+//     alert("Hisobingizda mablag' yetarli emas");
+// }
+
+// alert(`Balansda ${balance} so'm qoldi`)
